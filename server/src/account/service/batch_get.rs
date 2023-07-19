987bcb47c3d8b958d438c307f1c7d84fcd0c9892
custompro98 <mitchjoa@gmail.com>
2023-07-18 22:39:33 -0400

    Include users in batch get account

--- server/src/account/service/batch_get.rs
+++ server/src/account/service/batch_get.rs
@@ -2,6 +2,10 @@ use std::collections::HashMap;
 
 use tonic::{Response, Status, Code};
 
+use super::super::super::pb::user::User;
+
+use super::super::super::pb::user::batch_get_users_response::maybe_user::OptionalUser;
+
 use super::super::super::pb::account::batch_get_accounts_response::maybe_account::OptionalAccount;
 use super::super::super::pb::account::{batch_get_accounts_response::MaybeAccount, Account, BatchGetAccountsResponse};
 
@@ -24,22 +28,44 @@ impl Service {
         }
 
         let mut accounts_by_id: HashMap<i32, Account> = HashMap::new();
-        for account in accounts.unwrap() {
+        for account in accounts.to_owned().unwrap() {
             accounts_by_id.insert(account.id, account);
         }
 
-        let mut maybe_accounts: Vec<MaybeAccount> = Vec::new();
-        for account_id in account_ids {
-            match accounts_by_id.get(&account_id) {
-                Some(account) => maybe_accounts.push(MaybeAccount {
-                    optional_account: Some(OptionalAccount::Account(account.to_owned()))
-                }),
-                None => maybe_accounts.push(MaybeAccount { optional_account: None }),
+        let users = self.user_service
+            .batch_get(
+                &accounts
+                    .unwrap()
+                    .iter()
+                    .map(|acc| acc.owner_id )
+                    .collect()
+            ).await?;
+
+        let mut users_by_id: HashMap<i32, User> = HashMap::new();
+        for user in &users.get_ref().users {
+            if let Some(OptionalUser::User(user)) = &user.optional_user {
+                users_by_id.insert(user.id, user.to_owned());
             }
         }
 
-        /* let user = self.user_service.get(user.user_id).await?;
-        account.user = Some(user.get_ref().to_owned()); */
+        let mut maybe_accounts: Vec<MaybeAccount> = Vec::new();
+
+        for account_id in account_ids {
+            match accounts_by_id.get_mut(&account_id) {
+                Some(account) => {
+                    let user = users_by_id.get(&account.owner_id);
+
+                    if let Some(user) = user {
+                        account.owner = Some(user.to_owned());
+                    }
+
+                    maybe_accounts.push(MaybeAccount {
+                        optional_account: Some(OptionalAccount::Account(account.to_owned()))
+                    })
+                },
+                None => maybe_accounts.push(MaybeAccount { optional_account: None }),
+            }
+        }
 
         Ok(Response::new(BatchGetAccountsResponse {
             accounts: maybe_accounts,
